0b9d6972cd7260a3336e2efc86736322a0396313
abdulrafay553 <abdulrafaych53@gmail.com>
2024-06-11 11:52:14 +0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+        $("#divCart").hide()
+
+        $(document).ready(function(){
+
+          $("#btnadd").click(function(){
+            $("#divCart").slideDown()           
+          })
+
+          $("#z1").click(function(){
+             $("#divCart").slideUp()
+          })
+
+        })
